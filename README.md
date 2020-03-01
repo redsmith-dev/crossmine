b6aa96001e69b8211167ea326a34843154f7d85a
RedSmith <minecrafts_redsmith@hotmail.com>
2020-03-01 02:53:52 +0000

    Enchantment working. Checking with worldguard.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# crossmine
-Custom enchantment for TokenEnchant
+# CrossMine
+Custom enchantment for TokenEnchant that mines blocks in a cross formation.
+
+Permission to mine the blocks in the cross formation are checked against WorldGuard.
+Drops from blocks broken by the plugin are directly added to the Player's inventory and are calculated by the server (not my code) and the currently held tool.
+
+### Configuration
++ Place the jar in the enchants folder and the default config file will be written to disk if one does not exist yet.
++ Stop the server and edit all configurations if you want to. Each level will add a block to the length of the 3D cross.
++ Next time around TokenEnchant will see the config file for CrossMine.
--- src/redsmith/crossmine/CrossMine.java
+++ src/redsmith/crossmine/CrossMine.java
@@ -1,64 +1,144 @@
 package redsmith.crossmine;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.util.function.Consumer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
-import com.vk2gpz.tokenenchant.api.EnchantHandler;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
+import com.vk2gpz.tokenenchant.api.EnchantInfo;
 import com.vk2gpz.tokenenchant.api.InvalidTokenEnchantException;
+import com.vk2gpz.tokenenchant.api.PotionHandler;
 import com.vk2gpz.tokenenchant.api.TokenEnchantAPI;
 
 
-public class CrossMine extends EnchantHandler {	
+public class CrossMine extends PotionHandler {	
 	
 	public CrossMine(TokenEnchantAPI arg0) throws InvalidTokenEnchantException {
 		super(arg0);
 		super.loadConfig();
-		api = arg0;
 		
 		try {
-			checkConfig();
+			worldguard = WorldGuardPlugin.inst();
+			container = WorldGuard.getInstance().getPlatform().getRegionContainer();
 		} catch (Exception e) {
-			e.printStackTrace();
+			console.sendMessage(ChatColor.DARK_RED+"[CrossMine] - Failed to get instance of WorldGuard! ("+ChatColor.RED + e.toString() + ChatColor.DARK_RED+")");
 		}
-		addConflict("Explosive");
+
+		
+		checkConfig();
 	}
 	
-
+	
 	ConsoleCommandSender console = Bukkit.getConsoleSender();
-	TokenEnchantAPI api;
-
+	WorldGuardPlugin worldguard;
+	RegionContainer container;
 	
-	File configFile = new File("plugins"+File.separator+"TokenEnchant"+File.separator+"config.yml");
 	
-	void checkConfig() throws Exception {
-		
-		FileConfiguration config = api.getConfig();
-		
-		if (!config.isConfigurationSection("Enchants.CrossMine")) {
-			ConfigurationSection crossMineConfig = config.createSection("Enchants.CrossMine");
-			crossMineConfig.createSection("event_map").set("BlockBreakEvent", "LOW");
-			crossMineConfig.set("price", 10);
-			crossMineConfig.set("max", 1);
-			config.save(configFile);
-			
-			console.sendMessage("[CrossMine] - "+ChatColor.GREEN+"Added default values to TokeEnchant config.yml");
+	File configFile = new File("plugins"+File.separator+"TokenEnchant"+File.separator+"enchants"+File.separator+"CrossMine_config.yml");
+	
+	void checkConfig() {
+		if (!configFile.exists()) {
+			try {
+				Files.copy(getClass().getResourceAsStream("config.yml"), configFile.toPath());
+				console.sendMessage(ChatColor.GREEN+"[CrossMine] - Wrote default CrossMine_config.yml");
+				console.sendMessage(ChatColor.GREEN+"[CrossMine] - Reload might be needed to activate CrossMine.");
+			} catch (Exception e) {
+				console.sendMessage(ChatColor.DARK_RED+"[CrossMine] - Failed to write default config file! ("+ChatColor.RED + e.toString() + ChatColor.DARK_RED+")");
+			}
 		}
 		
 	}
 	
 	
-	@EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
-	public void onBlockBreak(BlockBreakEvent event) {
-		event.getPlayer().sendMessage("[CrossMine] - Mined "+ ChatColor.BOLD + ChatColor.RED + event.getBlock().getType().name());
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent event)
+	{
+		Player player = event.getPlayer();
+		Block block = event.getBlock();
+		EnchantInfo enchantment = hasCE(player);
 		
+		if (enchantment != null && enchantment.getHandler().getName().equalsIgnoreCase("CrossMine")) {
+			if (canBreak(player, block)) {
+				process(player, block, enchantment.getLevel());
+			}
+		}
+	}
+	
+	
+	private void process(Player player, Block block, int level) {
+		
+		Block[] scheduledBlocks = new Block[level*6];
+		Block[] candidateBlocks = new Block[level*6];
+		
+		for (int i = 0; i < level; i++) {
+			candidateBlocks[i*6]   = block.getRelative((i+1), 0, 0);
+			candidateBlocks[i*6+1] = block.getRelative(-(i+1), 0, 0);
+			candidateBlocks[i*6+2] = block.getRelative(0, (i+1), 0);
+			candidateBlocks[i*6+3] = block.getRelative(0, -(i+1), 0);
+			candidateBlocks[i*6+4] = block.getRelative(0, 0, (i+1));
+			candidateBlocks[i*6+5] = block.getRelative(0, 0, -(i+1));
+		}
+		
+		int scheduledCount = 0;
+		boolean[] results = batchCanBreak(player, candidateBlocks);
+		for (int i = 0; i < results.length; i++) {
+			if (results[i]) {
+				scheduledBlocks[scheduledCount] = candidateBlocks[i];
+				scheduledCount++;
+			}
+		}
+		
+		PlayerInventory inventory = player.getInventory();
+		
+		for (int i = 0; i < scheduledCount; i++) {
+			
+			scheduledBlocks[i].getDrops(inventory.getItemInMainHand()).iterator().forEachRemaining(new Consumer<ItemStack>(){
+				@Override
+				public void accept(ItemStack t) {
+					inventory.addItem(t);
+				}
+			});
+			
+			scheduledBlocks[i].setType(Material.AIR);
+		}
+	}
+	
+	
+	
+	public boolean canBreak(Player player, Block block) {
+		LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
+		RegionQuery query = container.createQuery();
+		
+		return query.testState(BukkitAdapter.adapt(block.getLocation()), localPlayer, Flags.BUILD);
+	}
+	
+	public boolean[] batchCanBreak(Player player, Block[] blocks) {
+		LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
+		RegionQuery query = container.createQuery();
+		
+		boolean[] results = new boolean[blocks.length];
+		for (int i = 0; i < blocks.length; i++) {
+			results[i] = query.testState(BukkitAdapter.adapt(blocks[i].getLocation()), localPlayer, Flags.BUILD);
+		}
+		return results;
 	}
 	 
 }
